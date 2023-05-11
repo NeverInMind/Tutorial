a1a90ea1f67190fdc8ca4de53b48f2a3383413d3
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-11 21:43:25 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+result = None
+operand = None
+operator = None
+wait_for_number = True
+while True:
+    if wait_for_number == True:
+        try:
+            i = 1 
+            if operator == '+':
+                result += operand
+            elif operator == '-':
+                result -= operand
+            elif operator == '/':
+                result /= operand
+            elif operator == '*':
+                result *= operand
+            operator = None
+            operand = int(input('Enter first number: '))
+            while True:
+                operator = input('Enter operator: ')
+                if operator == '=':
+                    print(f'Result: ' + str(result))
+                    wait_for_number = False
+                    break
+                elif operator not in '+-/*':
+                    print('It"s not + - / *. Pls try use numbers')
+                else:
+                    i += 1
+                    result = operand
+                    break
+        except:
+            print('It"s not a number. Pls try use + - / *')
+    else:
+        break
