f45df744eefea6a3a294e905b77e9e281c1add94
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-17 18:09:29 +0300

    Create ex-12.py

---
+++ module-04/ex-12.py
@@ -0,0 +1,46 @@
+from random import randint
+
+
+def get_random_password():
+    result = ""
+    count = 0
+    while count < 8:
+        random_symbol = chr(randint(40, 126))
+        result = result + random_symbol
+        count = count + 1
+    return result
+
+
+def is_valid_password(password):
+    has_upper = False
+    has_lower = False
+    has_num = False
+    for ch in password:
+        if "A" <= ch <= "Z":
+            has_upper = True
+        elif "a" <= ch <= "z":
+            has_lower = True
+        elif "0" <= ch <= "9":
+            has_num = True
+    if len(password) == 8 and has_upper and has_lower and has_num:
+        return True
+    return False
+
+
+def get_password():
+    check_numb = 0
+    while check_numb < 100:
+        check_numb += 1
+        new_password = get_random_password()
+        if is_valid_password(new_password):
+            return new_password
+        else:
+            continue
+    
+    
+    
+    
+        
+        
+        
+    
