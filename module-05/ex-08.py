b916c7cfe08763e6d5dd23fc0914b96081f20a6f
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-18 18:05:32 +0300

    Update ex-08.py

--- module-05/ex-08.py
+++ module-05/ex-08.py
@@ -1,5 +1,16 @@
-length = 2.75
-width = 1.75
-area = length * width
-show = f"With width {width} and length {length} of the room,\
-      its area is equal to {area}"
+grades = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
+students = {"Nick": "A", "Olga": "B", "Mike": "FX", "Anna": "C"}
+
+
+def formatted_grades(students):
+    res_arr = []
+    i = 1
+    for name, grade in students.items():
+        res_arr.append('{:>4}|{:<10}|{:^5}|{:^5}'.format(
+            i, name, grade, grades[grade]))
+        i += 1
+    return res_arr
+
+
+for el in formatted_grades(students):
+    print(el)
