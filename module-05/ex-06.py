789944dbbafd39f380a037b36de1e04c8dc17b9f
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-18 17:20:03 +0300

    Update ex-06.py

--- module-05/ex-06.py
+++ module-05/ex-06.py
@@ -1,11 +1,17 @@
 def is_spam_words(text, spam_words, space_around=False):
     for spam_word in spam_words:
         result = text.lower().find(spam_word)
+        check = False
         if result != -1:
-
-            return result
+            if space_around:
+                if text[result - 1] == ' ':
+                    return True
+                else:
+                    return False
+            else:
+                return True
         else:
             return False
 
 
-print(is_spam_words("Ты лох.", ["лох"]))
+print(is_spam_words('Молох бог ужасен.', ['лох']))
