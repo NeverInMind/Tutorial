5917214d895298dc0f9d1b4017d95d76674e7fba
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-19 16:56:42 +0300

    Update ex-09.py

--- module-05/ex-09.py
+++ module-05/ex-09.py
@@ -1,3 +1,9 @@
-a = -2 + 3j
-b = 4 + 2.1j
-result = a + b
+def formatted_numbers():
+    res_arr = ['|{:^10}|{:^10}|{:^10}|'.format('decimal', 'hex', 'binary')]
+    for i in range(0, 16):
+        res_arr.append('|{:<10d}|{:^10x}|{:>10b}|'.format(i, i, i))
+    return res_arr
+
+
+for el in formatted_numbers():
+    print(el)
