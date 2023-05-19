25dc5025ec759b9a375e6755034cc8f17363677b
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-19 17:12:36 +0300

    Update ex-10.py

--- module-05/ex-10.py
+++ module-05/ex-10.py
@@ -1,6 +1,25 @@
-a = -2
-b = 7
-c = -6
-D = b ** 2 - 4 * a * c 
-x1 = (-b + D ** 0.5) / (2 * a)
-x2 = (-b - D ** 0.5) / (2 * a)
+import re
+
+
+def find_word(text, word):
+    re_check = re.search(word, text)
+    if re_check is not None:
+        result = True
+        result_span = re_check.span()
+        result_str = re_check.group()
+        sample = {
+            'result': result,
+            'first_index': result_span[0],
+            'last_index': result_span[1],
+            'search_string': result_str,
+            'string': re_check.string
+        }
+    else:
+        sample = {
+            'result': False,
+            'first_index': None,
+            'last_index': None,
+            'search_string': result_str,
+            'string': re_check.string
+        }
+    return sample
