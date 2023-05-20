15bbe198188c097d5d8b3d5c2cc670651fd994a6
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-20 12:07:20 +0300

    Update ex-11.py

--- module-05/ex-11.py
+++ module-05/ex-11.py
@@ -1,2 +1,11 @@
-is_active = True
-is_delete = False
+import re
+
+
+def find_all_words(text, word):
+    result = re.findall(word, text, flags=re.I)
+    return result
+
+
+text = 'Python also pythOn and this also PyThoN'
+
+find_all_words(text, 'Python')
