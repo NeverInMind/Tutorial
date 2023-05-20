969ec559bc9157e5535bdb6f567f7895cce57d60
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-20 12:23:57 +0300

    Update ex-12.py

--- module-05/ex-12.py
+++ module-05/ex-12.py
@@ -1,4 +1,10 @@
-name = 'name'
-age = 21
-is_active = True
-subscription = None
+import re
+
+
+def replace_spam_words(text, spam_words):
+    for spam_word in spam_words:
+        text = re.sub(spam_word, '*' * len(spam_word), text, flags=re.I)
+    return text
+
+
+replace_spam_words('Молох бог ужасен.', 'лох')
