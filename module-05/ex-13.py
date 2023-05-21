0e584cce30472ec5344c356fdd1d5c0b06cf2b76
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-21 21:25:30 +0300

    Update ex-13.py

--- module-05/ex-13.py
+++ module-05/ex-13.py
@@ -1,5 +1,16 @@
-length = "2.75"
-width = "1.75"
-area = float(length) * float(width)
-show = f'With width {width} and length {length} of the room, its area is\
-      equal to {area}'
+import re
+
+
+def find_all_emails(text):
+    result = re.findall(r"\S\S+@\w+\.\w\w+", text)
+    return result
+
+
+print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org \
+                      first_last@iana.org \
+                first.middle.last@iana.or a@test.com abc111@test.com.net'))
+
+['Ima.Fool@iana.org', 'Fool@iana.org', 'first_last@iana.org',
+    'first.middle.last@iana.or', 'abc111@test.com']
+['Ima.Fool@iana.org', '1Fool@iana.org', 'first_last@iana.org',
+ 'first.middle.last@iana.or', 'abc111@test.com']
