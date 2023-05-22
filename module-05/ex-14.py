6a894a71120c08b53e21fdb83fa45db73f0f674e
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-22 21:16:19 +0300

    Update ex-14.py

--- module-05/ex-14.py
+++ module-05/ex-14.py
@@ -1,12 +1,7 @@
-import math
+import re
 
-RADIUS = 6371.01
 
-lat1 = math.radians(50.45)
-lon1 = math.radians(30.523)
-
-lat2 = math.radians(51.5072)
-lon2 = math.radians(-0.1275)
-
-distance = 6371.01 * math.acos(math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * 
-                               math.cos(lat2) * math.cos(lon1 - lon2))
+def find_all_phones(text):
+    pattern = r"\+{1}[0-9]{3}\([0-9]{2}\)[0-9]{3}-[0-9]{1}-[0-9]{3}|\+{1}[0-9]{3}\([0-9]{2}\)[0-9]{3}-[0-9]{2}-[0-9]{2}"
+    result = re.findall(pattern, text)
+    return result
