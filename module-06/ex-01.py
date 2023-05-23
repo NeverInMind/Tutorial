733bf6983dc47be9b23ed453bdfbe97beebc44ef
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-23 15:35:31 +0300

    Update ex-01.py

--- module-06/ex-01.py
+++ module-06/ex-01.py
@@ -1,9 +1,13 @@
-def real_len(text):
-    rule = ['\n', '\f', '\r', '\t', '\v']
-    for symb in rule:
-        text = text.replace(symb, '')
-    return len(text)
+def total_salary(path):
+    
+    
     
         
+            
+            
+                
+            
+            
     
+        
     
