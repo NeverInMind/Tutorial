f1fd47122747fb2e04f25dd50b83ab468e7d9179
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-23 15:25:21 +0300

    Update ex-15.py

--- module-05/ex-15.py
+++ module-05/ex-15.py
@@ -3,7 +3,8 @@ import re
 
 def find_all_links(text):
     result = []
-    iterator = re.finditer(r"", text)
+    pattern = r"https?://(?![a-zA-Z0-9_]*\.\.)[a-zA-Z0-9_.]+"
+    iterator = re.finditer(pattern, text)
     for match in iterator:
         result.append(match.group())
     return result
