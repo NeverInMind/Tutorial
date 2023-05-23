808c2a89d39877fee53943c6988a69b72238dd09
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-23 16:04:29 +0300

    Update ex-01.py

--- module-06/ex-01.py
+++ module-06/ex-01.py
@@ -1,13 +1,13 @@
 def total_salary(path):
-    
-    
-    
-        
-            
-            
-                
-            
-            
-    
-        
-    
+    pf = open(path, 'r')
+    sum = 0.0
+    while True:
+        line = pf.readline()
+        if not line:
+            break
+        sum += float(line.split(',')[1])
+    pf.close()
+    return sum
+
+
+print(total_salary(".\ex-01.txt"))
