5d9c698ec36ef058d5d787aa9e1ae34d3859539d
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-29 21:21:32 +0300

    Update ex-05.py

--- module-06/ex-05.py
+++ module-06/ex-05.py
@@ -1,40 +1,18 @@
-def sanitize_phone_number(phone):
-    new_phone = (
-        phone.strip()
-        .removeprefix("+")
-        .replace("(", "")
-        .replace(")", "")
-        .replace("-", "")
-        .replace(" ", "")
-    )
-    return new_phone
+def get_cats_info(path):
+    with open(path, 'r') as pf:
+        lines = pf.readlines()
+        new_arr = []
+        for i in lines:
+            data = i.split('\n')[0]
+            data_split = data.split(',')
+            new_dict = {'id': data_split[0],
+                        'name': data_split[1],
+                        'age': data_split[2]
+                        }
+            new_arr.append(new_dict)
+        return new_arr
 
 
-def get_phone_numbers_for_countries(list_phones):
-    rule_arr = {'81': 'JP', '65': 'SG', '886': 'TW', '380': 'UA'}
-    phone_obj = {
-        "UA": [],
-        "JP": [],
-        "TW": [],
-        "SG": []
-    }
-    for phone in list_phones:
-        new_ph = sanitize_phone_number(phone)
-        check_arr = [new_ph[0:2], new_ph[0:3]]
-        if check_arr[0] in rule_arr or check_arr[1] in rule_arr:
-            for key in rule_arr:
-                result = new_ph.find(key, 0, len(key))
-                if result == 0:
-                    phone_obj_key = rule_arr[key]
-                    require_arr = phone_obj[phone_obj_key]
-                    require_arr.append(new_ph)
+file_path = ".\ex-05.txt"
 
-        else:
-            require_arr = phone_obj['UA']
-            require_arr.append(new_ph)
-    return phone_obj
-
-
-phones = ['+81 321654987', '+65 132456789', '+886 123456789', '+380 123456789',
-          '+398 123654789']
-get_phone_numbers_for_countries(phones)
+get_cats_info(file_path)
