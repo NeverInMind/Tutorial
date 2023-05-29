b8ca841fa34dfabc08b3525c99b37cba7df5e25b
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-29 21:05:14 +0300

    Update ex-03.py

--- module-06/ex-03.py
+++ module-06/ex-03.py
@@ -1,5 +1,11 @@
-def sanitize_phone_number(phone):
-    rule = ['(', ')', '-', ' ', '+']
-    for item in rule:
-        phone = phone.replace(item, '')
-    return phone
+def read_employees_from_file(path):
+    pf = open(path, 'r')
+    lines = pf.readlines()
+    new_arr = []
+    for i in lines:
+        new_arr.append(i.split('\n')[0])
+    pf.close()
+
+
+file_path = ".\ex-02.txt"
+read_employees_from_file(file_path)
