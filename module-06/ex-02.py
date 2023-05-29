0717617a5dac2de1cec7e9955469e81a5f6c3876
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-29 21:05:07 +0300

    Update ex-02.py

--- module-06/ex-02.py
+++ module-06/ex-02.py
@@ -1,58 +1,13 @@
-articles_dict = [
-    {
-        "title": "Endless ocean waters.",
-        "author": "Jhon Stark",
-        "year": 2019,
-    },
-    {
-        "title": "Oceans of other planets are full of silver",
-        "author": "Artur Clark",
-        "year": 2020,
-    },
-    {
-        "title": "An ocean that cannot be crossed.",
-        "author": "Silver Name",
-        "year": 2021,
-    },
-    {
-        "title": "The ocean that you love.",
-        "author": "Golden Gun",
-        "year": 2021,
-    },
-]
+def write_employees_to_file(employee_list, path):
+    pf = open(path, 'w')
+    for i in employee_list:
+        for x in i:
+            pf.write(f'{x}\n')
+    pf.close()
 
 
-def find_articles(key, letter_case=False):
-    global articles_dict
-    new_art_arr = []
-    for art in articles_dict:
-        find_obj  = art.get('title') + ' ' + art.get('author')
-        if letter_case:
-            result = find_obj.find(key)
-            
-        else:
-            result = find_obj.lower().find(key.lower())
-        if result != -1:
-            new_art_arr.append(art)
-    return new_art_arr
+file_path = ".\ex-02.txt"
 
-    
-    
-        
-    
-        
-        
-        
-            
-        
-        
-            
-        
-        
-            
-        
-        
-            
-        
-            
-    
+new_list = [['Robert Stivenson,28', 'Alex Denver,30'], ['Drake Mikelsson,19']]
+
+write_employees_to_file(new_list, file_path)
