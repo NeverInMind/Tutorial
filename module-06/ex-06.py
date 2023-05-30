6106f92c8894e678bd651f5f077df1b00ccd7275
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-05-30 10:13:10 +0300

    Update ex-06.py

--- module-06/ex-06.py
+++ module-06/ex-06.py
@@ -1,16 +1,9 @@
-def is_spam_words(text, spam_words, space_around=False):
-    for spam_word in spam_words:
-        result = text.lower().find(spam_word)
-        if result != -1:
-            if space_around:
-                if text[result - 1] == ' ':
-                    return True
-                else:
-                    return False
-            else:
-                return True
-        else:
-            return False
+def get_recipe(path, search_id):
+    with open(path, 'r') as pf:
+        lines = pf.readlines()
 
 
-print(is_spam_words('Молох бог ужасен.', ['лох']))
+file_path = ".\ex-06.txt"
+src_id = '60b90c4613067a15887e1ae5'
+
+get_recipe(file_path, src_id)
