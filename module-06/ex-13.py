3036a3eaaebfd875a11c20d6d88d3d06dc124a7c
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-06 22:15:04 +0300

    Update ex-13.py

--- module-06/ex-13.py
+++ module-06/ex-13.py
@@ -1,17 +1,14 @@
-import re
+import shutil
 
 
-def find_all_emails(text):
-    pattern = r"[a-zA-Z]{1}[a-zA-z0-9._]{1,}@[a-zA-Z]+\.[a-zA-Z]{2,}"
-    result = re.findall(pattern, text)
-    return result
+def create_backup(path, file_name, employee_residence):
 
 
-print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org \
-                      first_last@iana.org \
-                first.middle.last@iana.or a@test.com abc111@test.com.net'))
 
-['Ima.Fool@iana.org', 'Fool@iana.org', 'first_last@iana.org',
-    'first.middle.last@iana.or', 'abc111@test.com']
-['Ima.Fool@iana.org', '1Fool@iana.org', 'first_last@iana.org',
- 'first.middle.last@iana.or', 'abc111@test.com']
+    with open(path + '/' + file_name, 'wb') as fh:
+        for key, value in employee_residence.items():
+            employee_data = f"{key} {value}\n"
+            fh.write(employee_data.encode())
+
+    archive_name = shutil.make_archive('backup_folder', 'zip', path)
+    return archive_name
