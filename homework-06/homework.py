82027399e2e5a6ba658e2cbbdcb4f821c7bd4a17
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-07 22:27:20 +0300

    Update homework.py

--- homework-06/homework.py
+++ homework-06/homework.py
@@ -1,3 +1,8 @@
 from normalize import normalize
+from pathlib import Path
 
-normalize('Мотлох_1;№.txt')
+p = Path('.\Мотлох_1;№')
+file_tree = [x for x in p.iterdir() if x.is_dir()]
+for child in file_tree:
+    file_tree2 = [x for x in child.iterdir() if x.is_dir()]
+    print(file_tree2)
