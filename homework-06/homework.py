7a34444de27e4e5a173215a11b84bc6ee7655c59
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-08 23:04:12 +0300

    sorted

--- homework-06/homework.py
+++ homework-06/homework.py
@@ -1,5 +1,6 @@
 from normalize import normalize
 import sys
+import shutil
 from pathlib import Path
 
 CATEGORIES = {
@@ -15,6 +16,8 @@ def move_file(path: Path, root_dir: Path, cat: str) -> None:
     target_dir = root_dir.joinpath(cat)
     if not target_dir.exists():
         target_dir.mkdir()
+    new_name = target_dir.joinpath(f"{normalize(path.stem)}{path.suffix}")
+    path.replace(new_name)
 
 
 def get_categories(path: Path) -> str:
@@ -22,12 +25,30 @@ def get_categories(path: Path) -> str:
     for cat, exts in CATEGORIES.items():
         if ext in exts:
             return cat
-        return 'Other'
+    return 'Other'
 
 
 def sort_folder(path: Path) -> None:
     for item in path.glob('**/*'):
-        print(item.stem)
+        if item.is_file():
+            cat = get_categories(item)
+            if cat == 'archives':
+                new_path = path.joinpath(cat).joinpath(item.stem)
+                print(new_path)
+                unpack_archive(item, new_path)
+                move_file(item, path, cat)
+            else:
+                move_file(item, path, cat)
+        elif item.name in CATEGORIES:
+            pass
+        else:
+            new_name = item.parent.joinpath(normalize(item.name))
+            item.replace(new_name)
+
+
+def unpack_archive(path: Path, new_path: Path):
+    shutil.unpack_archive(path, new_path)
+    pass
 
 
 def main():
