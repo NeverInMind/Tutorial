72e9b36152514c0a22b8254a0cc69cf7c30725f9
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-12 20:38:22 +0300

    Update ex-01.py

--- module-07/ex-01.py
+++ module-07/ex-01.py
@@ -1,13 +1,24 @@
-def total_salary(path):
-    pf = open(path, 'r')
-    sum = 0.0
-    while True:
-        line = pf.readline()
-        if not line:
-            break
-        sum += float(line.split(',')[1])
-    pf.close()
-    return sum
+from setuptools import setup
 
 
-print(total_salary(".\ex-01.txt"))
+def do_setup(args_dict):
+    setup(name=args_dict['name'],
+          version=args_dict['version'],
+          description=args_dict['description'],
+          url=args_dict['url'],
+          author=args_dict['author'],
+          author_email=args_dict['author_email'],
+          license=args_dict['license'],
+          packages=args_dict['packages'])
+
+
+args = {
+    "name": "useful",
+    "version": "1",
+    "description": "Very useful code",
+    "url": "http://github.com/dummy_user/useful",
+    "author": "Flying Circus",
+    "author_email": "flyingcircus@example.com",
+    "license": "MIT",
+    "packages": ["useful"],
+}
