85c0979458024bc106830e1954be4da1ca4c3097
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-13 17:03:25 +0300

    Update ex-05.py

--- module-07/ex-05.py
+++ module-07/ex-05.py
@@ -2,10 +2,19 @@ import re
 
 
 def capital_text(s):
-    chekc = re.search(' ', s)
-    print(s[5])
+    s = s.strip().capitalize()
+    check_rule = ['!', '?', '.']
+    change = False
+    for i in range(0, len(s)):
+        check_let = s[i]
+        if check_let != " " and change:
+            s = s[:i] + check_let.upper() + s[i+1:]
+            change = False
+        if check_let in check_rule:
+            change = True
+            continue
+    return s
 
 
-test = 'test test!test.test?test '
-
+test = ' alert. boss! oh'
 capital_text(test)
