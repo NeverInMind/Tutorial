ece7bafc6fc702e25c55e7523fc428d7842a708e
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-15 23:08:32 +0300

    Update ex-09.py

--- module-07/ex-09.py
+++ module-07/ex-09.py
@@ -1,7 +1,17 @@
-def is_equal_string(utf8_string, utf16_string):
-    utf8_string = utf8_string.decode('utf-8')
-    utf16_string = utf16_string.decode('utf-16')
-    if utf16_string == utf8_string:
-        return True
-    else:
-        return False
+def all_sub_lists(data):
+    new_list = [[]]
+    data_length = len(data)
+    if data_length <= 1:
+        return new_list
+    for item in range(0, len(data)):
+        while data_length != 0:
+            data_length -= 1
+            check_data = data[item:data_length+1]
+            if check_data != []:
+                new_list.append(check_data)
+        data_length = len(data)
+    return sorted(new_list, key=len)
+
+
+test = [1, 2, 3, 4]
+all_sub_lists(test)
