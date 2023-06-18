f701dad27b7651dc2b19849d26396f9017c26865
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-18 20:19:31 +0300

    Update ex-11.py

--- module-07/ex-11.py
+++ module-07/ex-11.py
@@ -1,11 +1,24 @@
-def get_credentials_users(path):
-    with open(path, 'rb') as pf:
-        new_arr = []
-        data = pf.readlines()
-        for i in data:
-            new_arr.append(i.strip().decode())
-        print(new_arr)
+def sequence_buttons(string):
+    vocabul = {'1': '.,?!:',
+               '2': 'ABC',
+               '3': 'DEF',
+               '4': 'GHI',
+               '5': 'JKL',
+               '6': 'MNO',
+               '7': 'PQRS',
+               '8': 'TUV',
+               '9': 'WXYZ',
+               '0': ' '}
+    res_string = ''
+    for letter in string:
+        letter = letter.capitalize()
+        for keys, values in vocabul.items():
+            res = values.find(letter)
+            if res != -1:
+                res_string += keys * (res + 1)
+    return res_string
 
 
-file_path = ".\\user_info.txt"
-get_credentials_users(file_path)
+test = 'Hello Vovan!;'
+
+sequence_buttons(test)
