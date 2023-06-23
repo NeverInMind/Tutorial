6c4dfda48ed800aaca0638c5e0b602571c0a3a3c
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-23 21:09:24 +0300

    add ex-13

--- module-07/ex-13.py
+++ module-07/ex-13.py
@@ -1,14 +1,10 @@
-import shutil
-
-
-def create_backup(path, file_name, employee_residence):
-
-
-
-    with open(path + '/' + file_name, 'wb') as fh:
-        for key, value in employee_residence.items():
-            employee_data = f"{key} {value}\n"
-            fh.write(employee_data.encode())
-
-    archive_name = shutil.make_archive('backup_folder', 'zip', path)
-    return archive_name
+def get_employees_by_profession(path, profession):
+    
+        
+    
+    
+        
+            
+    
+    
+get_employees_by_profession('.\output-12.txt', '')
