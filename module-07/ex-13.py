c162dff0ce9f5744d0a06fd9c1c55abbe6a8e421
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-06-26 12:39:38 +0300

    new edit

--- module-07/ex-13.py
+++ module-07/ex-13.py
@@ -1,10 +1,15 @@
 def get_employees_by_profession(path, profession):
-    
-        
-    
-    
-        
-            
-    
-    
-get_employees_by_profession('.\output-12.txt', '')
+    new_arr = []
+    with open(path, 'r')as pf:
+        res = pf.readlines()
+        for item in res:
+            new = item.strip()
+            finder = new.find(profession)
+            if finder != -1:
+                new = new.replace(profession, '').strip()
+                new_arr.append(new)
+    result = " ".join(new_arr)
+    return result
+
+
+get_employees_by_profession('.\output-13.txt', 'courier')
--- module-07/ex-14.py
+++ module-07/ex-14.py
@@ -1,5 +1,14 @@
-import shutil
+def to_indexed(source_file, output_file):
+    with open(source_file, 'r') as pf:
+        with open(output_file, 'w') as ph:
+            filt = pf.readlines()
+            counter = 0
+            for item in filt:
+                item = f'{counter}: {item}'
+                counter += 1
+                ph.write(item)
 
 
-def unpack(archive_path, path_to_unpack):
-    shutil.unpack_archive(archive_path, path_to_unpack)
+old = '.\output-14.txt'
+new = '.\output-14_new.txt'
+to_indexed(old, new)
